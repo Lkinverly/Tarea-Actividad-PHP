f7adda47e342e30c685348f01c1690ddb36bef3c
Kinverly <kinlopez2024@gmail.com>
2025-09-01 19:47:12 -0600

    Primer commit - proyecto php_empresa

---
+++ crud_empleado.php
@@ -0,0 +1,52 @@
+<?php
+// Archivo = crud_empleado.php
+if (!empty($_POST)) {
+    $txt_id = utf8_decode($_POST["txt_id"]);
+    $txt_codigo = utf8_decode($_POST["txt_codigo"]);
+    $txt_nombres = utf8_decode($_POST["txt_nombres"]);
+    $txt_apellidos = utf8_decode($_POST["txt_apellidos"]);
+    $txt_direccion = utf8_decode($_POST["txt_direccion"]);
+    $txt_telefono = utf8_decode($_POST["txt_telefono"]);
+    $txt_fn = utf8_decode($_POST["txt_fn"]);
+    $drop_puesto = utf8_decode($_POST["drop_puesto"]);
+    include("datos_conexion.php");
+    $db_conexion = mysqli_connect ($db_host, $db_usr, $db_pass, $db_nombre);
+    $sql = "";
+    // AGREGAR
+    if (isset($_POST['btn_agregar'])) {
+        $sql = "INSERT INTO empleados (codigo, nombres, apellidos, direccion, telefono, fecha_nacimiento, id_puesto)
+                VALUES ('$txt_codigo', '$txt_nombres', '$txt_apellidos', '$txt_direccion', '$txt_telefono', '$txt_fn', $drop_puesto)";
+    }
+    // MODIFICAR
+    if (isset($_POST['btn_modificar'])) {
+        $sql = "UPDATE empleados SET
+                codigo = '$txt_codigo',
+                nombres = '$txt_nombres',
+                apellidos = '$txt_apellidos',
+                direccion = '$txt_direccion',
+                telefono = '$txt_telefono',
+                fecha_nacimiento = '$txt_fn',
+                id_puesto = $drop_puesto
+                WHERE id_empleado = $txt_id";
+    }
+    // ELIMINAR
+    if (isset($_POST['btn_eliminar'])) {
+        $sql = "DELETE FROM empleados WHERE id_empleado = $txt_id";
+    }
+    // NUEVO (solo limpia, no hace query)
+    if (isset($_POST['btn_nuevo'])) {
+        $sql = "";
+    }
+    // Solo ejecutar query si realmente hay SQL
+    if (!empty($sql)) {
+        if ($db_conexion->real_query($sql) === true) {
+            $db_conexion->close();
+            header("Location: /php_empresa");
+            exit;
+        } else {
+            echo "Error: " . $sql . "<br>" . $db_conexion->error;
+            $db_conexion->close();
+        }
+    }
+}
+?>
---
+++ datos_conexion.php
@@ -0,0 +1,6 @@
+<?php
+ $db_host="localhost:3306";
+ $db_usr = "kin";
+ $db_pass ="123456!";
+ $db_nombre="desarrollo_web";
+?>
